6c825e7fe7482726a83d39476665e6c123e70b73
sobhan83sefidkar <mongodbacc10@gmail.com>
2025-04-17 19:22:12 +0330

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="pqSXn1iU7tYDiVkG9tyt8XPGuhuDcsAskioO6yi6xM8" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
